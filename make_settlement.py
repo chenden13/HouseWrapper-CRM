bbae3bafa3a8b9870c80604e914f00e2a642be8f
chenden13 <chenden8@gmail.com>
2026-08-31 17:35:11 +0800

    chore: clean up build artifacts

--- make_settlement.py
+++
@@ -1,4 +0,0 @@
-import os
-p = " src/components/AccessorySettlementPage.tsx\
-with open(p, \w\, encoding=\utf-8\) as f: f.write(os.environ[\SETTLEMENT_CODE\])
-print(\done\)
--- writer.py
+++
@@ -1,4 +0,0 @@
-import base64, sys
-path = sys.argv[1]
-b64 = sys.argv[2]
-open(path, " wb\).write(base64.b64decode(b64.encode(\ascii\)))
